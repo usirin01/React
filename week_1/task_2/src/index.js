1fa83676303c61d26f7bfefc375484094b190462
usirin01 <usirin2021@gmail.com>
2022-02-17 23:46:40 +0100

    header and footer were created

---
+++ week_1/task_2/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/header';
+import Card from './components/card';
+import Footer from './components/footer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+Header();
