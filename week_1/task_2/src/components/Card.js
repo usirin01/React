2cbf4c289d5284b3c5e6d93004fc93c9a064f22a
usirin01 <usirin2021@gmail.com>
2022-02-19 21:35:57 +0100

    new arranged

--- week_1/task_2/src/components/Card.js
+++ week_1/task_2/src/components/Card.js
@@ -10,8 +10,7 @@ const Card = () => {
                 <a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>
             </div>
-        
-
+            
     );
 };
 
--- week_1/task_2/src/components/Header.js
+++ week_1/task_2/src/components/Header.js
@@ -11,10 +11,10 @@ const Header = () => {
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
                 </div>
