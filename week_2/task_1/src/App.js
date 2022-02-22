6a35bcc04410f3a06e7dfe4800283e1bbd7e098d
usirin01 <usirin2021@gmail.com>
2022-02-22 21:26:18 +0100

    added DigitalCLock.js

--- week_2/task_1/src/App.js
+++ week_2/task_1/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Body />
+      <DigitalClock />
     </div>
   );
 }
---
+++ week_2/task_1/src/components/DigitalClock.js
@@ -0,0 +1,40 @@
+function DigitalClock(){
+  
+    var todayHTML = document.getElementById('today-js');
+    var monthHTML = document.getElementById('month-js');  
+    var timeHTML = document.getElementById('time-js');
+    var dateHTML = document.getElementById('date-js'); 
+    var yearHTML = document.getElementById('year-js');  
+
+    var d = new Date();
+    var year = 		d.getFullYear();
+    var month = 	d.getMonth();
+    var date =		d.getDate();
+    date = addZero(date);
+    var hours = 	d.getHours();
+    hours = addZero(hours);
+    var minutes = d.getMinutes();
+    minutes = addZero(minutes);
+    var seconds = d.getSeconds();
+    seconds = addZero(seconds);
+    var today =   d.getDay(); 
+    var dayName = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  
+    timeHTML.innerHTML =  hours + ' : ' + minutes + ' : ' + seconds; 
+    todayHTML.innerHTML = dayName[today];
+    dateHTML.innerHTML =  date;
+    monthHTML.innerHTML = monthName[month];
+    yearHTML.innerHTML =  year;
+  
+  
+}
+
+function addZero(i) {
+  if ( i < 10 ) {
+    i = '0' + i;
+  }
+  return i;
+}
+
+ setInterval(digitalClock, 1000); 
