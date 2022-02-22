5d740fa2af1cc7ad4d7d22a5fa5f968db6106e05
usirin01 <usirin2021@gmail.com>
2022-02-22 21:19:38 +0100

    added Body.js

---
+++ week_2/task_1/src/App.js
@@ -0,0 +1,12 @@
+import Body from './components/Body';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ week_2/task_1/src/components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Body = () => {
+    return (
+        <div id="digital-clock">
+            <p id="time-js"></p>
+            <p id="today-js"></p>
+            <p id="date-js"></p>
+            <p id="month-js"></p>
+            <p id="year-js"></p>
+        </div>
+    )
+}
+
+export default Body;
