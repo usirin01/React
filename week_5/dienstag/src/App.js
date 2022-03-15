5ac5e77be3dcab63ad9fdb55664b7445e9571823
usirin01 <usirin2021@gmail.com>
2022-03-15 21:41:49 +0100

    json server is working

--- week_5/dienstag/src/App.js
+++ week_5/dienstag/src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
+import {useState, useEffect} from "react";
 
 const App = () => {
+
+  const [userList, setUserList] = useState(null)
+
+  useEffect(() => {
+    getUserList()
+  })
+
+  const getUserList = async () => {
+    const response = await fetch("http://localhost:3001/posts");
+    const data = await response.json();
+    setUserList(data);
+  };
+  if (userList !== null) {
+    console.log("user list", userList);
+  }
   return (
     <div className="App">
      
