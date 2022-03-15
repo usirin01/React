b42398d6cec62cebe53b9d525d9d0366e051f23b
usirin01 <usirin2021@gmail.com>
2022-03-15 22:43:39 +0100

    the last arrange has already added

--- week_5/react_router/src/App.js
+++ week_5/react_router/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import Navs from './components/Navs'
+import About from './components/About'
+import Home from './components/Home'
+import Contact from './components/Contact'
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <BrowserRouter>
+      <React.Fragment>
+        <Navs />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </React.Fragment>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ week_5/react_router/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Navs = () => {
+  return (
+    <div id="navbar">
+      <ul className="nav bg-info">
+          <li className="nav-link">
+           <Link to="/">HOME</Link> 
+          </li>
+          <li className="nav-link">
+           <Link to="/About">ABOUT</Link>  
+          </li>
+          <li className="nav-link">
+          <Link to="/Contact">CONTACT</Link>  
+          </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navs
