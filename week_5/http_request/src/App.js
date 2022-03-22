3fb7e4a1d1a8237801f6cc1cbb7b0bdd10a955e5
usirin01 <usirin2021@gmail.com>
2022-03-23 00:27:38 +0100

    App.js was added to http_request

--- week_5/http_request/src/App.js
+++ week_5/http_request/src/App.js
@@ -1,9 +1,24 @@
 
-import './App.css';
+import { useState } from 'react';
+import Form from './components/Form';
+import Person from './components/Person';
 
 function App() {
+
+  const [addperson, setAddperson] = useState([]);
+
+  const saveform = (person) => {
+    setAddperson([...addpeople,person]);
+  };
+
   return (
     <div className="App">
+      <div className="container px-4">
+        <div className="row gx-5">
+          <Form saveform = {saveform}/>
+          <Person addperson = {addperson}/>  
+        </div>
+      </div>
       
     </div>
   );
