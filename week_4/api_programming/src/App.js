e9e4e84f0df419624ed1ffa33cecf3c8bde0f69a
usirin01 <usirin2021@gmail.com>
2022-03-27 18:33:49 +0200

    wheather API last changing was committed

--- week_4/api_programming/src/App.js
+++ week_4/api_programming/src/App.js
@@ -5,21 +5,22 @@ function App() {
 const [apiList, setApiList] = useState([]);
 
 useEffect (() => {
-  getApiList();
+  async function getData() {
+    const response = await fetch(url)
+    const data = await response.json();
+    console.log(data);
+    setApiList(data);
+  }
+  getData();
 },[]);
 
 const url = "https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=97e97a059caa4862b96ba8009259d0cb&include=minutely"
 
-const getApiList = async () => {
-  const response = await fetch(url)
-  const data = await response.json();
-  console.log(data);
-  setApiList(data);
-}
+
 
   return (
     <div className="App">
-      
+      ...data
     </div>
   );
 }
