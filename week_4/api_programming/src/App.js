eb0acfe72b8c912d2d06d1d182d2d7dc80706a20
usirin01 <usirin2021@gmail.com>
2022-03-27 18:26:02 +0200

    wheather API last arranged

--- week_4/api_programming/src/App.js
+++ week_4/api_programming/src/App.js
@@ -1,9 +1,5 @@
 import {useState, useEffect } from 'react'
 
-function url(){
-  return "https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=97e97a059caa4862b96ba8009259d0cb&include=minutely"
-}
-
 function App() {
 
 const [apiList, setApiList] = useState([]);
@@ -12,10 +8,13 @@ useEffect (() => {
   getApiList();
 },[]);
 
+const url = "https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=97e97a059caa4862b96ba8009259d0cb&include=minutely"
+
 const getApiList = async () => {
   const response = await fetch(url)
   const data = await response.json();
   console.log(data);
+  setApiList(data);
 }
 
   return (
