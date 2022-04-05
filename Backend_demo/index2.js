770216720e77ce22b9faa454ace666133b2a4a91
usirin01 <usirin2021@gmail.com>
2022-04-05 23:23:49 +0200

    backend second push

--- Backend_demo/index2.js
+++ Backend_demo/index2.js
@@ -4,6 +4,7 @@ const port = 3002;
 const hostname = "127.0.0.1"
 
 const server = http.createServer((reguest, response) => {
+    res.statusCode = 202;
     response.write("Hi");
     response.end();
 });
